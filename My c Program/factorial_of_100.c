5ebbcd0d5c5ae7566a508ec96bee09919dbcb19e
Mehul Andani <63387662+Mehul175@users.noreply.github.com>
2020-10-01 14:48:01 +0530

    Update factorial_of_100.c

--- My c Program/factorial_of_100.c
+++ My c Program/factorial_of_100.c
@@ -1,38 +1,3 @@
-Skip to content
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@Mehul175 
-Learn Git and GitHub without any code!
-Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
-
-
-Mehul175
-/
-Project-Euler
-forked from RisingLight/Project-Euler
-0
-082
-Code
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-Settings
-Project-Euler/C/20.Factorial_Digit_Sum/Mehul175.c
-@Mehul175
-Mehul175 Update Mehul175.c
-Latest commit 3705b17 20 minutes ago
- History
- 1 contributor
-43 lines (37 sloc)  1.01 KB
-  
 // Euler 20
 #include <stdio.h>
 #define MAX_DIGIT 200
@@ -76,15 +41,4 @@ int main()
 
     return 0;
 }
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+
